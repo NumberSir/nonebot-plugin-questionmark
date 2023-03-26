a535b294b5c1c8700773a636cd543e5ec3472394
NumberSir <52584526+NumberSir@users.noreply.github.com>
2023-03-26 22:29:10 +0800

    v0.1.1

--- nonebot_plugin_questionmark/__init__.py
+++ nonebot_plugin_questionmark/__init__.py
@@ -7,5 +7,7 @@ question = on_message()
 
 @question.handle()
 async def _(event: MessageEvent):
-    if event.message.extract_plain_text() in {"?", "？"}:
-        await question.send("¿")
+    mark = event.message.extract_plain_text().strip()
+    if all(_ in {"?", "？", "¿"} for _ in set(mark)):
+        mark = mark.replace("¿", "d").replace("?", "¿").replace("？", "¿").replace("d", "?")
+        await question.send(mark)
